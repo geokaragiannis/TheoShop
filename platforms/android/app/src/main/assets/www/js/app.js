51b6ce42a467974097aa1be7d0cbf1aa1f62d0ac
George Karagiannis <gekaragi@ucsc.edu>
2018-05-04 15:13:15 +0300

    switched to subpage brancg

---
+++ platforms/android/app/src/main/assets/www/js/app.js
@@ -0,0 +1,105 @@
+(function () {
+
+  var data = {
+    parent_items: ['Drinks', 'Food', 'Products'],
+    children_items: {
+      'Drinks': ['Hot Coffees', 'Hot Teas', 'Hot Drinks', 'Cold Coffees', 'Cold Drinks', 'Iced Teas'],
+      'Food': ['Breakfast', 'Bakery', 'Lunch', 'Snacks', 'Sweets', 'Yogurt'],
+      'Products': ['Honey', 'Cream', 'Feta Cheese']
+    },
+
+    sub_items: {
+      'hot_coffees': ['Capuccino', 'Espresso', 'Latte', 'Americano']
+    },
+    // drinks: ['Hot Coffees', 'Hot Teas', 'Hot Drinks', 'Cold Coffees', 'Cold Drinks', 'Iced Teas'],
+    // food: ['Breakfast', 'Bakery', 'Lunch', 'Snacks', 'Sweets', 'Yogurt']
+    item_selected: null
+  };
+
+
+  function init() {
+
+    // Init F7 Vue Plugin
+    Vue.use(Framework7Vue)
+
+    // Init Page Components
+    Vue.component('page-main', {
+      template: '#page-main',
+      data: function(){
+        return data;
+      },
+      methods: {
+        goToCart: function(){
+
+        },
+
+        goToSubMenu: function(is){
+          data.item_selected = is
+        }
+
+      }
+    })
+    Vue.component('login-screen', {
+      template: '#login-screen'
+    })
+    Vue.component('sub-menu', {
+      template: '#sub-menu',
+      data: function(){
+        return data;
+      },
+      methods: {
+
+      }
+    })
+    Vue.component('cart', {
+      template: '#cart'
+    })
+    Vue.component('page-dynamic-routing', {
+      template: '#page-dynamic-routing'
+    })
+
+
+    // Init App
+    new Vue({
+      el: '#app',
+      // Init Framework7 by passing parameters here
+      framework7: {
+        root: '#app',
+        /* Uncomment to enable Material theme: */
+        // material: true,
+        routes: [
+          {
+            path: '/main/',
+            component: 'page-main'
+          },
+          {
+            path: '/login/',
+            component: 'login-screen'
+          },
+          {
+            path: '/sub-menu/',
+            component: 'sub-menu'
+          },
+          {
+            path: '/cart/',
+            component: 'cart'
+          },
+          {
+            path: '/dynamic-route/blog/:blogId/post/:postId/',
+            component: 'page-dynamic-routing'
+          }
+        ],
+      }
+    });
+
+  }
+
+
+
+  // Handle device ready event
+  // Note: You may want to check out the vue-cordova package on npm for cordova specific handling with vue - https://www.npmjs.com/package/vue-cordova
+  document.addEventListener('deviceready', init, false)
+
+
+
+})();
