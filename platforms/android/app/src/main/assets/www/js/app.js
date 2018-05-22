e1a99ca06442285ed1a337d9e65a508b32dbf6bb
George Karagiannis <gekaragi@ucsc.edu>
2018-05-22 12:37:06 +0300

    ajax call is being done

--- platforms/android/app/src/main/assets/www/js/app.js
+++ platforms/android/app/src/main/assets/www/js/app.js
@@ -1,388 +1,12 @@
 (function () {
 
+  var app_data = {
+    big_list: []
+  }
+
   var data = {
 
-    // big_list: [
-    //   {name:'Drinks' ,is_leaf: false, id:1 , p_id:0, level: 1, children: [{name:'Coffee' ,is_leaf: false, id:10 , p_id:1, level: 2, children:[{name:'Cap' , is_leaf: true, id:20 , p_id:10, level: 3},{name:'Esp' , is_leaf: true, id:21 , p_id:10, level: 3}] }, {name:'Tea' , is_leaf: false, id:11 , p_id:1, level: 2, children:[{name:'Engligh' , is_leaf: true, id:22 , p_id:11, level: 3},{name:'Greek' , is_leaf: true, id:23 , p_id:11, level: 3}]}]},
-    //   {name: 'Food',is_leaf: false, id: 2, p_id: 0, level: 1, children: [{name:'Breakfast' ,is_leaf: false, id:30 , p_id:2, level: 2, children:[{name:'Omelate' , is_leaf: false, id:40 , p_id:30, level: 3, children: [{name:'Omelate2' , is_leaf: true, id:80 , p_id:40, level: 4}, {name:'Omelate3', is_leaf: true, id:81 , p_id:40, level: 4}, {name:'Omelate4' , is_leaf: true, id:82 , p_id:40, level: 4}]},{name:'Bagel' , is_leaf: false, id:41 , p_id:30, level: 3, children: [
-		// 		{name:'Bagel2' , is_leaf: true, id:90 , p_id:41, level: 4},
-		// 		{name:'Bagel3', is_leaf: true, id:92 , p_id:41, level: 4},
-		// 		{name:'Bagel4' , is_leaf: true, id:92 , p_id:41, level: 4}
-		// 	]}]}, {name:'Lunch' , is_leaf: false, id:31 , p_id:2, level: 2, children:[{name:'Mousakas' , is_leaf: true, id:42 , p_id:31, level: 3},{name:'Pastitsio' , is_leaf: true, id:43 , p_id:31, level: 3}]}]}
-    // ],
-
-    big_list: [
-              	{
-              		"id": 15,
-              		"parentId": null,
-              		"descr": "FOOD",
-              		"typ": "CAT",
-              		"price": null,
-              		"compCd": "001",
-              		"lev": 1,
-              		"isLeaf": 0,
-              		"child": [
-              			{
-              				"id": 17,
-              				"parentId": 15,
-              				"descr": "BREAKFAST",
-              				"typ": "CAT",
-              				"price": null,
-              				"compCd": "001",
-              				"lev": 2,
-              				"isLeaf": 0,
-              				"child": [
-              					{
-              						"id": 18,
-              						"parentId": 17,
-              						"descr": "HOT",
-              						"typ": "CAT",
-              						"price": null,
-              						"compCd": "001",
-              						"lev": 3,
-              						"isLeaf": 0,
-              						"child": [
-              							{
-              								"id": 10000,
-              								"parentId": 18,
-              								"descr": "OMELATE",
-              								"typ": "ITEM",
-              								"price": 3,
-              								"compCd": "001",
-              								"lev": 4,
-              								"isLeaf": 1
-              							},
-              							{
-              								"id": 10001,
-              								"parentId": 18,
-              								"descr": "TOAST",
-              								"typ": "ITEM",
-              								"price": 2,
-              								"compCd": "001",
-              								"lev": 4,
-              								"isLeaf": 1
-              							}
-              						]
-              					},
-              					{
-              						"id": 19,
-              						"parentId": 17,
-              						"descr": "COLD",
-              						"typ": "CAT",
-              						"price": null,
-              						"compCd": "001",
-              						"lev": 3,
-              						"isLeaf": 0,
-              						"child": [
-              							{
-              								"id": 10002,
-              								"parentId": 19,
-              								"descr": "BAGEL",
-              								"typ": "ITEM",
-              								"price": 2,
-              								"compCd": "001",
-              								"lev": 4,
-              								"isLeaf": 1
-              							}
-              						]
-              					}
-              				]
-              			},
-              			{
-              				"id": 20,
-              				"parentId": 15,
-              				"descr": "LUNCH",
-              				"typ": "CAT",
-              				"price": null,
-              				"compCd": "001",
-              				"lev": 2,
-              				"isLeaf": 0,
-              				"child": [
-              					{
-              						"id": 10003,
-              						"parentId": 20,
-              						"descr": "MOUSAKAS",
-              						"typ": "ITEM",
-              						"price": 5,
-              						"compCd": "001",
-              						"lev": 3,
-              						"isLeaf": 1
-              					},
-              					{
-              						"id": 10005,
-              						"parentId": 20,
-              						"descr": "PASTITSIO",
-              						"typ": "ITEM",
-              						"price": 6,
-              						"compCd": "001",
-              						"lev": 3,
-              						"isLeaf": 1
-              					}
-              				]
-              			},
-              			{
-              				"id": 25,
-              				"parentId": 15,
-              				"descr": "DINNER",
-              				"typ": "CAT",
-              				"price": null,
-              				"compCd": "001",
-              				"lev": 2,
-              				"isLeaf": 0,
-              				"child": [
-              					{
-              						"id": 10007,
-              						"parentId": 25,
-              						"descr": "STEAK",
-              						"typ": "ITEM",
-              						"price": 7,
-              						"compCd": "001",
-              						"lev": 3,
-              						"isLeaf": 1
-              					},
-              					{
-              						"id": 10008,
-              						"parentId": 25,
-              						"descr": "PIZZA",
-              						"typ": "ITEM",
-              						"price": 7,
-              						"compCd": "001",
-              						"lev": 3,
-              						"isLeaf": 1
-              					}
-              				]
-              			},
-              			{
-              				"id": 26,
-              				"parentId": 15,
-              				"descr": "SNACKS",
-              				"typ": "CAT",
-              				"price": null,
-              				"compCd": "001",
-              				"lev": 2,
-              				"isLeaf": 0,
-              				"child": [
-              					{
-              						"id": 10009,
-              						"parentId": 26,
-              						"descr": "CAPRESE SANDWICH",
-              						"typ": "ITEM",
-              						"price": 4,
-              						"compCd": "001",
-              						"lev": 3,
-              						"isLeaf": 1
-              					},
-              					{
-              						"id": 10010,
-              						"parentId": 26,
-              						"descr": "CHIPS",
-              						"typ": "ITEM",
-              						"price": 3,
-              						"compCd": "001",
-              						"lev": 3,
-              						"isLeaf": 1
-              					},
-              					{
-              						"id": 10011,
-              						"parentId": 26,
-              						"descr": "GREEK CHOCOLATE",
-              						"typ": "ITEM",
-              						"price": 2,
-              						"compCd": "001",
-              						"lev": 3,
-              						"isLeaf": 1
-              					}
-              				]
-              			}
-              		]
-              	},
-              	{
-              		"id": 15,
-              		"parentId": null,
-              		"descr": "FOOD",
-              		"typ": "CAT",
-              		"price": null,
-              		"compCd": "001",
-              		"lev": 1,
-              		"isLeaf": 0,
-              		"child": [
-              			{
-              				"id": 17,
-              				"parentId": 15,
-              				"descr": "BREAKFAST",
-              				"typ": "CAT",
-              				"price": null,
-              				"compCd": "001",
-              				"lev": 2,
-              				"isLeaf": 0,
-              				"child": [
-              					{
-              						"id": 18,
-              						"parentId": 17,
-              						"descr": "HOT",
-              						"typ": "CAT",
-              						"price": null,
-              						"compCd": "001",
-              						"lev": 3,
-              						"isLeaf": 0,
-              						"child": [
-              							{
-              								"id": 10000,
-              								"parentId": 18,
-              								"descr": "OMELATE",
-              								"typ": "ITEM",
-              								"price": 3,
-              								"compCd": "001",
-              								"lev": 4,
-              								"isLeaf": 1
-              							},
-              							{
-              								"id": 10001,
-              								"parentId": 18,
-              								"descr": "TOAST",
-              								"typ": "ITEM",
-              								"price": 2,
-              								"compCd": "001",
-              								"lev": 4,
-              								"isLeaf": 1
-              							}
-              						]
-              					},
-              					{
-              						"id": 19,
-              						"parentId": 17,
-              						"descr": "COLD",
-              						"typ": "CAT",
-              						"price": null,
-              						"compCd": "001",
-              						"lev": 3,
-              						"isLeaf": 0,
-              						"child": [
-              							{
-              								"id": 10002,
-              								"parentId": 19,
-              								"descr": "BAGEL",
-              								"typ": "ITEM",
-              								"price": 2,
-              								"compCd": "001",
-              								"lev": 4,
-              								"isLeaf": 1
-              							}
-              						]
-              					}
-              				]
-              			},
-              			{
-              				"id": 20,
-              				"parentId": 15,
-              				"descr": "LUNCH",
-              				"typ": "CAT",
-              				"price": null,
-              				"compCd": "001",
-              				"lev": 2,
-              				"isLeaf": 0,
-              				"child": [
-              					{
-              						"id": 10003,
-              						"parentId": 20,
-              						"descr": "MOUSAKAS",
-              						"typ": "ITEM",
-              						"price": 5,
-              						"compCd": "001",
-              						"lev": 3,
-              						"isLeaf": 1
-              					},
-              					{
-              						"id": 10005,
-              						"parentId": 20,
-              						"descr": "PASTITSIO",
-              						"typ": "ITEM",
-              						"price": 6,
-              						"compCd": "001",
-              						"lev": 3,
-              						"isLeaf": 1
-              					}
-              				]
-              			},
-              			{
-              				"id": 25,
-              				"parentId": 15,
-              				"descr": "DINNER",
-              				"typ": "CAT",
-              				"price": null,
-              				"compCd": "001",
-              				"lev": 2,
-              				"isLeaf": 0,
-              				"child": [
-              					{
-              						"id": 10007,
-              						"parentId": 25,
-              						"descr": "STEAK",
-              						"typ": "ITEM",
-              						"price": 7,
-              						"compCd": "001",
-              						"lev": 3,
-              						"isLeaf": 1
-              					},
-              					{
-              						"id": 10008,
-              						"parentId": 25,
-              						"descr": "PIZZA",
-              						"typ": "ITEM",
-              						"price": 7,
-              						"compCd": "001",
-              						"lev": 3,
-              						"isLeaf": 1
-              					}
-              				]
-              			},
-              			{
-              				"id": 26,
-              				"parentId": 15,
-              				"descr": "SNACKS",
-              				"typ": "CAT",
-              				"price": null,
-              				"compCd": "001",
-              				"lev": 2,
-              				"isLeaf": 0,
-              				"child": [
-              					{
-              						"id": 10009,
-              						"parentId": 26,
-              						"descr": "CAPRESE SANDWICH",
-              						"typ": "ITEM",
-              						"price": 4,
-              						"compCd": "001",
-              						"lev": 3,
-              						"isLeaf": 1
-              					},
-              					{
-              						"id": 10010,
-              						"parentId": 26,
-              						"descr": "CHIPS",
-              						"typ": "ITEM",
-              						"price": 3,
-              						"compCd": "001",
-              						"lev": 3,
-              						"isLeaf": 1
-              					},
-              					{
-              						"id": 10011,
-              						"parentId": 26,
-              						"descr": "GREEK CHOCOLATE",
-              						"typ": "ITEM",
-              						"price": 2,
-              						"compCd": "001",
-              						"lev": 3,
-              						"isLeaf": 1
-              					}
-              				]
-              			}
-              		]
-              	}
-              ],
-
-
+    big_list: [],
     parent_list: [],
     children_list: [],
     parent_stack: [],
@@ -424,7 +48,7 @@
     var $$ = Dom7;
 
     // Init F7 Vue Plugin
-    Vue.use(Framework7Vue, Framework7)
+    Vue.use(Framework7Vue, Framework7);
 
     // Init Page Components
     Vue.component('page-main', {
@@ -439,12 +63,12 @@
 
         goToSubMenu: function(is){
           // console.log('test map_canvas ', $$('#map_canvas').hasClass('pipa'));
-          console.log('aa ', this.$f7)
-          data.parent_list = data.big_list
-          data.parent_stack.push(data.big_list)
+          // console.log('aa ', this.$f7)
+          console.log('main page big_list ', app_data.big_list)
+          data.parent_list = app_data.big_list
+          console.log('parent_list: ', data.parent_list)
+          data.parent_stack.push(app_data.big_list)
         },
-
-
 
       }
     })
@@ -467,7 +91,7 @@
         return data;
       },
       methods: {
-        
+
         goToSame: function(item){
 
           // if the child of the item selected is a leaf, then
@@ -681,15 +305,43 @@
       },
 
     })
-
-
     // Init App
     new Vue({
       el: '#app',
+      data: function(){
+        return app_data
+      },
       methods: {
         makeMap: function(){
 
         }
+      },
+      mounted: function(){
+        console.log('app mounted')
+        // make ajax call
+        url = "http://demo.qnr.com.gr:7003/EshopWs/api/eshop/eshopinfo"
+        success =
+        user = "eshop|002"
+        pass = "123"
+
+        $$.ajax({
+          type: "GET",
+          dataType: "json",
+          url: url,
+          beforeSend: function (xhr) {
+              xhr.setRequestHeader ("Authorization", "Basic " + btoa(user + ":" + pass));
+          },
+          success: function(data){
+            console.log('json: ', data)
+            app_data.big_list = data
+            console.log('big_list: ', app_data.big_list)
+          },
+          error: function() {
+            console.log('error in ajax call')
+          }
+
+        });
+
       },
       // Init Framework7 by passing parameters here
       framework7: {
